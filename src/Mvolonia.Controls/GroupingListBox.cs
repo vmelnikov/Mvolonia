cba60d3386bd4b7e72340abe9e1199f2f49bc8e0
vadim <vmelnikov@gmail.com>
2021-05-01 16:29:15 +0300

    add GroupStyle class

--- src/Mvolonia.Controls/GroupingListBox.cs
+++ src/Mvolonia.Controls/GroupingListBox.cs
@@ -1,6 +1,9 @@
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Generators;
+using Avalonia.Controls.Templates;
 using Mvolonia.Controls.Generators;
 using Mvolonia.Controls.Utils;
 
@@ -8,8 +11,15 @@ namespace Mvolonia.Controls
 {
     public class GroupingListBox : ListBox
     {
-        
-        /// <inheritdoc/>
+        /// <summary>
+        /// The collection of GroupStyle objects that describes the display of
+        /// each level of grouping.  The entry at index 0 describes the top level
+        /// groups, the entry at index 1 describes the next level, and so forth.
+        /// If there are more levels of grouping than entries in the collection,
+        /// the last entry is used for the extra levels.
+        /// </summary>
+        public ObservableCollection<GroupStyle> GroupStyle { get; } = new ObservableCollection<GroupStyle>();
+
         protected override IItemContainerGenerator CreateItemContainerGenerator()
         {
             return new GroupItemContainerGenerator(
