6b427a83c35f3347479ab9b63215b7959290345c
vadim <vmelnikov@gmail.com>
2021-05-17 20:02:36 +0300

    simplify GroupItem

--- src/Mvolonia.Controls/GroupItem.cs
+++ src/Mvolonia.Controls/GroupItem.cs
@@ -1,68 +1,21 @@
-using System.Collections;
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Controls.Generators;
 using Avalonia.Controls.Presenters;
 using Avalonia.Controls.Primitives;
-using Avalonia.Controls.Templates;
 using Mvolonia.Controls.Collections;
-using Mvolonia.Controls.Generators;
 
 namespace Mvolonia.Controls
 {
     
-    public class GroupItem : TemplatedControl, IGroupItem, IItemsPresenterHost
+    public class GroupItem : TemplatedControl, IGroupItem
     {
         private Control _header;
-        
-        private IEnumerable _items;
-        private IItemContainerGenerator _itemContainerGenerator;
         private CollectionViewGroup _viewGroup;
-        private StackPanel _panel;
-
-        /// <summary>
-        /// Gets or sets the data template used to display the items in the control.
-        /// </summary>
-        public IDataTemplate ItemTemplate { get; private set; }
-
-
+        
         /// <inheritdoc/>
-        public bool IsEmpty => Panel.Children.Count == 0;
+        public bool IsEmpty => Panel?.Children?.Count == 0;
 
-        public IPanel Panel => _panel;
-
-        /// <summary>
-        /// Gets the items presenter control.
-        /// </summary>
-        public IItemsPresenter Presenter
-        {
-            get;
-            protected set;
-        }
-
-        /// <summary>
-        /// Gets the <see cref="IItemContainerGenerator"/> for the control.
-        /// </summary>
-        public IItemContainerGenerator ItemContainerGenerator
-        {
-            get
-            {
-                if (_itemContainerGenerator != null) 
-                    return _itemContainerGenerator;
-                _itemContainerGenerator = CreateItemContainerGenerator();
-
-                if (_itemContainerGenerator is null) 
-                    return null;
-                
-                _itemContainerGenerator.ItemTemplate = ItemTemplate;
-                _itemContainerGenerator.Materialized += (_, e) => OnContainersMaterialized(e);
-                _itemContainerGenerator.Dematerialized += (_, e) => OnContainersDematerialized(e);
-                _itemContainerGenerator.Recycled += (_, e) => OnContainersRecycled(e);
-
-                return _itemContainerGenerator;
-            }
-        }
-
+        public IPanel Panel { get; private set; }
+        
         internal CollectionViewGroup ViewGroup
         {
             get => _viewGroup;
@@ -71,70 +24,23 @@ namespace Mvolonia.Controls
                 if (_viewGroup == value)
                     return;
                 _viewGroup = value;
-                _items = ViewGroup.Items;
                 UpdateHeader();
             }
         }
-
-        private void OnContainersRecycled(ItemContainerEventArgs itemContainerEventArgs)
+        
+        public void AddControl(IControl control)
         {
-            var groupingListBox = GetParent<GroupingListBox>(); 
-            groupingListBox?.ChildContainersRecycled(itemContainerEventArgs);
-        }
-
-        private void OnContainersDematerialized(ItemContainerEventArgs itemContainerEventArgs)
-        {
-            var groupingListBox = GetParent<GroupingListBox>(); 
-            groupingListBox?.ChildContainersDematerialized(itemContainerEventArgs);
-        }
-
-        private void OnContainersMaterialized(ItemContainerEventArgs itemContainerEventArgs)
-        {
-            var groupingListBox = GetParent<GroupingListBox>(); 
-            groupingListBox?.ChildContainersMaterialized(itemContainerEventArgs);
-        }
-
-        public new  void ApplyTemplate()
-        {
-            base.ApplyTemplate();
-        }
-
-        private T GetParent<T>() where T: class
-        {
-            IControl control = this;
-            while (!(control.Parent is null))
-            {
-                control = control.Parent;
-                if (control is T t)
-                    return t;
-            }
-
-            return null;
-        }
-
-        private IItemContainerGenerator CreateItemContainerGenerator()
-        {
-            return new GroupItemContainerGenerator(
-                this, 
-                ListBoxItem.ContentProperty,
-                ListBoxItem.ContentTemplateProperty);
+            Panel?.Children.Add(control);
         }
 
         protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
         {
             _header =  e.NameScope.Find<Control>("PART_Header");
-            _panel =  e.NameScope.Find<StackPanel>("PART_Panel");
+            Panel =  e.NameScope.Find<StackPanel>("PART_Panel");
             UpdateHeader();
             base.OnApplyTemplate(e);
         }
 
-        protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
-        {
-            var itemsControl = GetParent<ItemsControl>(); 
-            if (!(itemsControl is null))
-                ItemTemplate = itemsControl.ItemTemplate;
-            base.OnAttachedToVisualTree(e);
-        }
 
         private void UpdateHeader()
         {
@@ -156,14 +62,18 @@ namespace Mvolonia.Controls
             contentPresenter.DataContext = ViewGroup;
         }
 
-        public void RegisterItemsPresenter(IItemsPresenter presenter)
-        {
-            Presenter = presenter;
-        }
 
-        public void AddControl(IControl control)
+        private T GetParent<T>() where T: class
         {
-            Panel?.Children.Add(control);
+            IControl control = this;
+            while (!(control.Parent is null))
+            {
+                control = control.Parent;
+                if (control is T t)
+                    return t;
+            }
+
+            return null;
         }
     }
 }
